3e6a531766d3dda3350d6aee6a8b84d8f12a4fcc
Paweł Dąbała <pawel.dabala@global.com>
2019-04-23 12:47:32 +0200

    added templates for car and clients

--- myrent/myrent/myrentcar/urls.py
+++ myrent/myrent/myrentcar/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     #ex: /cars/
     path('cars/', views.cars, name='cars'),
     #ex: /cars/5/
-    path('<int:car_id>/', views.car, name='car')
+    path('cars/<int:car_id>/', views.car, name='car'),
+    #ex: /clients/
+    path('clients/', views.clients, name='clients'),
+    #ex: /client/3/
+    path('clients/<int:client_id>', views.client, name='client')
 ]
--- myrent/myrent/myrentcar/views.py
+++ myrent/myrent/myrentcar/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Car
+from .models import Car, Client
 
 # Create your views here.
 
@@ -11,10 +11,18 @@ from .models import Car
 
 def cars(request):
     cars = Car.objects.all()
-    print(cars)
     return render(request, 'myrentcar/cars.html', {'cars': cars})
 
 
 def car(request, car_id):
     car = get_object_or_404(Car, pk=car_id)
     return render(request, 'myrentcar/car.html', {'car': car})
+
+def clients(request):
+    clients = Client.objects.all()
+    return render(request,'myrentcar/clients.html', {'clients': clients})
+
+def client(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+    return render(request, 'myrentcar/client.html', {'client': client})
+
