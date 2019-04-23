e726befd0b8f14709f43626978ccc04807058a55
Paweł Dąbała <pawel.dabala@global.com>
2019-04-23 11:54:27 +0200

    added cars template

--- myrent/myrent/myrentcar/urls.py
+++ myrent/myrent/myrentcar/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'myrentcar'
 urlpatterns = [
-    path('', views.index, name='index')
+    #ex: /cars/
+    path('cars/', views.cars, name='cars'),
+    #ex: /cars/5/
+    path('<int:car_id>/', views.car, name='car')
 ]
--- myrent/myrent/myrentcar/views.py
+++ myrent/myrent/myrentcar/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Car
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hell, world, You're at the rent car index")
+# def index(request):
+#     return HttpResponse("Hell, world, You're at the rent car index")
 
 
+def cars(request):
+    cars = Car.objects.all()
+    print(cars)
+    return render(request, 'myrentcar/cars.html', {'cars': cars})
+
+
+def car(request, car_id):
+    car = get_object_or_404(Car, pk=car_id)
+    return render(request, 'myrentcar/car.html', {'car': car})
